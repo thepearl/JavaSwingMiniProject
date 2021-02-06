9e8d7b8f20764d0155899257ad6113c9d197a2a3
Ghazi Tozri <ghazi.tozri98@gmail.com>
2021-02-06 11:31:53 +0100

    JMenu DONE

--- src/FenetreGestionnaire.java
+++ src/FenetreGestionnaire.java
@@ -1,6 +1,38 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class FenetreGestionnaire extends JFrame
 {
-    
+    public FenetreGestionnaire()
+    {
+        // TOP BAR Configs
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("Fichier");
+        JMenu articleMenu = new JMenu("Article");
+
+        JMenuItem exploreItem = new JMenuItem("Explorer");
+        fileMenu.add(exploreItem);
+        JMenuItem importItem = new JMenuItem("Importer");
+        fileMenu.add(importItem);
+        JMenuItem quitItem = new JMenuItem("Quitter");
+        fileMenu.add(quitItem);
+        JMenuItem newItem = new JMenuItem("Nouveau");
+        articleMenu.add(newItem);
+        JMenuItem modifyItem = new JMenuItem("Modifier");
+        articleMenu.add(modifyItem);
+        JMenuItem deleteItem = new JMenuItem("Supprimer");
+        articleMenu.add(deleteItem);
+
+        menuBar.add(fileMenu);
+        menuBar.add(articleMenu);
+
+        setJMenuBar(menuBar);
+
+
+        setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Gestionnaire des articles");
+        this.pack();
+        this.setVisible(true);
+    }
 }
---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+import java.io.IOException;
+
+/**
+ *
+ * @author Ghazi Tozri
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.io.IOException
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        new FenetreGestionnaire() ;
+    }
+}
